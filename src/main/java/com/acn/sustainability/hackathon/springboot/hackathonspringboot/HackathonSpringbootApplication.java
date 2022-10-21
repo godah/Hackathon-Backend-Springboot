7ff319b85d9770b5dba64c7b447e368381fa45d7
geann.marozzi <geann.marozzi@CPX-DKV8794JTCM.dir.svc.accenture.com>
2022-10-21 14:49:31 -0300

    Teste

--- src/main/java/com/acn/sustainability/hackathon/springboot/hackathonspringboot/HackathonSpringbootApplication.java
+++ src/main/java/com/acn/sustainability/hackathon/springboot/hackathonspringboot/HackathonSpringbootApplication.java
@@ -11,7 +11,7 @@ public class HackathonSpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HackathonSpringbootApplication.class, args);
-
+		System.out.println("teste");
 	}
 
 	@Bean
